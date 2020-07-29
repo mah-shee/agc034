c142a53c5f1bbe662f3cccf7249080eb4040d645
masa <masatoshi.sboard@gmail.com>
2020-07-29 19:44:11 +0900

    Done a

--- src/bin/a.rs
+++ src/bin/a.rs
@@ -4,19 +4,43 @@ use proconio::{fastout, input};
 #[fastout]
 fn main() {
     input! {
-        n: usize,
+        _: usize,
         a: usize,
         b: usize,
         c: usize,
         d: usize,
-        s: Chars,
+        mut s: Chars,
     }
-    // c < dのとき、aがbを追い越すことはないので、b,
-    // aがそれぞれゴールに辿り着けるかどうかを判定すればいい
 
-    // c > dのとき、aがbを追い越すことになる。
-    // 追い越すことができる条件は、b < d の間に岩のない空間が最小3つ連続していること。
-    // そのタイミングでaはbを追いこすことができ、そこからはa,
-    // bのがそれぞれゴールにたどり着けるかどうかを判定する
-    // ゴールにたどり着ける条件は、岩のない空間が2つ連続しないこと
+    if c > d && between(b, d, &s) == false {
+        // c > dのとき、aがbを追い越すことになる。
+        // 追い越すことができる条件は、b < d の間に岩のない空間が最小3つ連続していること。
+        println!("No");
+        return;
+    }
+    // そのタイミングでaはbを追いこすことができ、
+    // そこからa, bのがそれぞれゴールにたどり着けるかどうかを判定する
+    if goal(b, d, &s) == true && goal(a, c, &s) == true {
+        println!("Yes");
+    } else {
+        println!("No");
+    }
+}
+// ゴールにたどり着ける条件は、現在いる地点からゴールの間に岩のない空間が2つ連続しないこと
+fn goal(start: usize, goal: usize, map: &Vec<char>) -> bool {
+    for i in start - 1..goal {
+        if map[i] == '#' && map[i + 1] == '#' {
+            return false;
+        }
+    }
+    true
+}
+
+fn between(b: usize, d: usize, map: &Vec<char>) -> bool {
+    for i in b - 1..d {
+        if map[i - 1] == '.' && map[i] == '.' && map[i + 1] == '.' {
+            return true;
+        }
+    }
+    false
 }
